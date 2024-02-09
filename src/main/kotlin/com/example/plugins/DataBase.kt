37fb6acc89cd11a10638b6b433c0ae08905b680b
user <a@>
2024-02-09 20:50:41 +0300

    initial

--- src/main/kotlin/com/example/plugins/DataBase.kt
+++ src/main/kotlin/com/example/plugins/DataBase.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.sql.Database
 
 fun Application.configureDatabase() {
     Database.connect(
-        url = "jdbc:postgresql://localhost:5433/hotel_test2?characterEncoding=utf8&useUnicode=true",
+        url = "jdbc:postgresql://localhost:5433/hotel?characterEncoding=utf8&useUnicode=true",
         user = "testLogin",
         driver = "org.postgresql.Driver",
         password = "test"
